912e46147a51561556c888103d0bcddf08600bb7
Wa'Lyncia Brown <walyncia.brown@students.cau.edu>
2024-01-02 13:44:32 -0500

    add main file with algorithm

---
+++ Main.py
@@ -0,0 +1,13 @@
+#This program is dedicated to a recieving password and validating the stregnth of the password against a set of rules.
+
+#ALGORITHM
+
+#1. Get password from user
+#2. Add all rules
+#3. Check password against rules
+#4. Give validation feedback to user
+#5. If not, prompt user for a stronger password
+#6. Give validation feedback to user
+#7. If not, repeat steps 5 and 6
+#8. If, say password is OK
+#9. End program
